618708d6849d49b968157cf88254d8fdb4efc093
Paweł Łęczycki <pawel@msd.info.pl>
2021-03-22 11:23:30 +0100

    Dobre praktyki pisania testów jednostkowych

---
+++ ClassLibrary.Test/StringConcatenatorUnitTest.cs
@@ -0,0 +1,87 @@
+using System;
+using Xunit;
+
+namespace ClassLibrary.Test
+{
+    public class StringConcatenatorUnitTest : IDisposable
+    {
+        public StringConcatenatorUnitTest()
+        {
+            _stringConcatenator = new StringConcatenator();
+        }
+
+        private readonly StringConcatenator  _stringConcatenator;
+
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+
+
+        private StringConcatenator CreateDefaultStringConcatenator()
+        {
+            return new StringConcatenator();
+        }
+
+        [Fact]
+        public void Concat_ThrowsArgumentNullException_WhenArgumentIsNull()
+        {
+            //Arrage
+            var stringConcatenator = CreateDefaultStringConcatenator();
+
+            //Act
+            Action action = () => stringConcatenator.Concat(null);
+
+            //Assert
+            var argumentNullException = Assert.Throws<ArgumentNullException>(action);
+            Assert.Equal("value", argumentNullException.ParamName);
+        }
+
+        [Fact]
+        public void Concat_ResultsSameStringAsInput_WhenPassingSingleSign()
+        {
+            //Arrage
+            //var stringConcatenator = new StringConcatenator();
+
+            //Act
+            _stringConcatenator.Concat("0");
+            var result = _stringConcatenator.ToString();
+
+            //Assert
+            Assert.Equal("0", result);
+        }
+
+        [Fact]
+        public void Concat_ThrowsOverflowException_WhenPassingLongString()
+        {
+            //Arrage
+            var stringConcatenator = new StringConcatenator();
+            const string OVER_MAXIMUM_LENGTH = "ala ma kota i psa";
+
+            //Act
+            Action action = () => stringConcatenator.Concat(OVER_MAXIMUM_LENGTH);
+
+            //Assert
+            Assert.Throws<OverflowException>(action);
+        }
+
+
+
+        [Theory]
+        [InlineData("a", "a")]
+        [InlineData("A", "a")]
+        [InlineData("AbC", "abc")]
+        public void Concat_RestursExpected_WhenPassingStrings(string input, string expected)
+        {
+            //Arrage
+            var stringConcatenator = new StringConcatenator();
+
+            //Act
+            stringConcatenator.Concat(input);
+            var result = stringConcatenator.ToString();
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ ClassLibrary/StringConcatenator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class StringConcatenator
+    {
+        private string _value = "";
+        public const int MAX_LENGTH = 10;
+
+        public StringConcatenator Concat(string value)
+        {
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+
+            if (_value.Length + value.Length > MAX_LENGTH)
+                throw new OverflowException();
+            Execture(value);
+
+            return this;
+        }
+
+        private void Execture(string value)
+        {
+            //if(DateTime.Now.DayOfWeek <= DayOfWeek.Friday)
+            //    _value += value.ToUpper();
+            //else
+                _value += value.ToLower();
+
+        }
+
+        public override string ToString()
+        {
+            return _value;
+        }
+    }
+}
