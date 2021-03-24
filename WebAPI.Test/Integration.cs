d63a5e8a475ef0916c8049eaa0011aa257988354
Paweł Łęczycki <pawel@msd.info.pl>
2021-03-24 16:15:29 +0100

    Przykład testu integracyjnego

---
+++ WebAPI.Test/Integration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WebAPI.Test.Controllers
+{
+    public class Integration : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public Integration(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/Orders")]
+        [InlineData("/WeatherForecast")]
+        public async Task Get(string url)
+        {
+            var clinet = _factory.CreateClient();
+
+            var response = await clinet.GetAsync(url);
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
